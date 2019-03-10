f2fdacf20289eb444d91cff417a830fbc3b07d3d
Ingols-D <ing18006@byui.edu>
2019-03-09 23:38:31 -0700

    JSON 509

--- lesson-9/js/towndata.js
+++ lesson-9/js/towndata.js
@@ -30,16 +30,7 @@ function showTowns(jsonObj) {
             var myPara4 = document.createElement('p');
             var myImg = document.createElement('img');
 
-            if (townObj[i].name == "Preston") {
-                myImg.setAttribute('src', 'https://cdn.glitch.com/0e6459e2-7f45-4e2b-8100-f606b11bdadc%2Fdownload.jpeg?1551738255453');
-                myImg.setAttribute('alt', 'Preston Image');
-            } else if (townObj[i].name == "Soda Springs") {
-                myImg.setAttribute('src', 'https://cdn.glitch.com/0e6459e2-7f45-4e2b-8100-f606b11bdadc%2Fdownload.jpeg?1551738255453');
-                myImg.setAttribute('alt', 'Soda Springs');
-            } else if (townObj[i].name == "Fish Haven") {
-                myImg.setAttribute('src', 'https://cdn.glitch.com/0e6459e2-7f45-4e2b-8100-f606b11bdadc%2Fdownload.jpeg?1551738255453');
-                myImg.setAttribute('alt', 'Fish Haven');
-            }
+  
 
             myH2.textContent = townObj[i].name;
             myPara1.textContent = townObj[i].motto;
