bd2a3a9ae2ec8e4749bcdcdf4ab94beb64da896c
Ingols-D <ing18006@byui.edu>
2019-03-16 14:54:05 -0600

    weather info1

---
+++ lesson-10/js/currentweather.js
@@ -0,0 +1,10 @@
+var weatherObject = new XMLHttpRequest ();
+weatherObject.open('GET','http://api.openweathermap.org/data/2.5/weather?zip=83263,us&appid=428ce573a70fb23421cf1bbd73ba6e08',true);
+
+weatherObject.send();
+
+weatherObject.onload = function(){
+    var weatherInfo = JSON.parse(weatherObject.responseText);
+    console.log(weatherInfo);
+
+}//end of the function
