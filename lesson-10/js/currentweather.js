bf98681d87bd342e1894230a9237acc927d62a2c
Ingols-D <ing18006@byui.edu>
2019-03-20 07:30:19 -0600

    windchill 1

--- lesson-10/js/currentweather.js
+++ lesson-10/js/currentweather.js
@@ -13,6 +13,20 @@ weatherObject.onload = function(){
     document.getElementById('windSpeed').innerHTML = weatherInfo.wind.speed;
 
     var iconcode = weatherInfo.weather[0].icon;
-    var icon_path="http://openweathermap.org/img/w/" +iconcode + ".png";
-    
+    var icon_path="//openweathermap.org/img/w/" +iconcode + ".png";
+    document.getElementById("weather_icon").src=icon_path;
+   
+    function winddChill {
+    var { tempF, speed } = newFunction();
+        var f = 35.74 + (0.6215 * tempF)-35.75 * Math.pow(speed, 0.16) + (0.4275 * tempF * (Math.pow(speed, 0.16)));
+        var windChill = f.toFixed(2)+"&deg;";
+          document.getElementById(windChill).innerHTML;
+
+function newFunction() {
+        var tempF = currentTemp;
+        var speed = windSpeed;
+        return { tempF, speed };
+    }
+}
+        
 }//end of the function
