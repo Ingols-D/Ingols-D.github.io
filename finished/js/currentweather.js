e4f1139c82b1e41477132921332d7bc6eb412386
Ingols-D <ing18006@byui.edu>
2019-03-23 09:24:06 -0600

    finished1

---
+++ finished/js/currentweather.js
@@ -0,0 +1,31 @@
+// CURRENT WEATHER SCRIPT
+var weatherObject = new XMLHttpRequest ();
+weatherObject.open('GET','//api.openweathermap.org/data/2.5/weather?zip=83263,us&appid=428ce573a70fb23421cf1bbd73ba6e08&units=imperial',true);
+
+weatherObject.send();
+
+weatherObject.onload = function(){
+    var weatherInfo = JSON.parse(weatherObject.responseText);
+    console.log(weatherInfo);
+
+    document.getElementById('weather').innerHTML = weatherInfo.weather["0"].description;
+
+    var currentTemp = parseFloat(document.getElementById('currentTemp').innerHTML = weatherInfo.main.temp);
+    var windSpeed = parseFloat(document.getElementById('windSpeed').innerHTML = weatherInfo.wind.speed);
+
+    
+    var tempF = currentTemp;
+    var speed = windSpeed;
+    var f= windChill(tempF, speed);
+
+    document.getElementById("outputDiv").innerHTML = f.toFixed(2);
+
+    function windChill (tempF, speed){
+       
+        var f = 35.74 + (0.6215 * tempF)-35.75 * Math.pow(speed, 0.16) + (0.4275 * tempF * (Math.pow(speed, 0.16)));
+        return f;
+
+
+    
+    }}
+    // end of CURRENT WEATHER
---
+++ finished/js/fiveday.js
@@ -0,0 +1,54 @@
+var forecastObject = new XMLHttpRequest();
+forecastObject.open('GET', '//api.openweathermap.org/data/2.5/forecast?id=5604473&appid=428ce573a70fb23421cf1bbd73ba6e08&units=imperial', true);
+
+forecastObject.send();
+
+forecastObject.onload = function () {
+ var forecastInfo = JSON.parse(forecastObject.responseText)   
+   console.log(forecastInfo); 
+//Day 1
+    document.getElementById("tempHi1").innerHTML = forecastInfo.list[1].main.temp_max;
+        
+    document.getElementById("tempLo1").innerHTML = forecastInfo.list[1].main.temp_min;
+        
+    document.getElementById("description1").innerHTML = forecastInfo.list[1].weather["0"].description;
+
+    document.getElementById("speed1").innerHTML = forecastInfo.list[1].wind.speed;
+    
+//Day 2
+     document.getElementById("tempHi2").innerHTML = forecastInfo.list[2].main.temp_max;
+        
+    document.getElementById("tempLo2").innerHTML = forecastInfo.list[2].main.temp_min;
+        
+    document.getElementById("description2").innerHTML = forecastInfo.list[2].weather["0"].description;
+
+    document.getElementById("speed2").innerHTML = forecastInfo.list[2].wind.speed;
+    
+//Day 3
+     document.getElementById("tempHi3").innerHTML = forecastInfo.list[3].main.temp_max;
+        
+    document.getElementById("tempLo3").innerHTML = forecastInfo.list[3].main.temp_min;
+        
+    document.getElementById("description3").innerHTML = forecastInfo.list[3].weather["0"].description;
+
+    document.getElementById("speed3").innerHTML = forecastInfo.list[3].wind.speed;
+    
+//Day 4
+     document.getElementById("tempHi4").innerHTML = forecastInfo.list[4].main.temp_max;
+        
+    document.getElementById("tempLo4").innerHTML = forecastInfo.list[4].main.temp_min;
+        
+    document.getElementById("description4").innerHTML = forecastInfo.list[4].weather["0"].description;
+
+    document.getElementById("speed4").innerHTML = forecastInfo.list[4].wind.speed;
+    
+//Day 5
+     document.getElementById("tempHi5").innerHTML = forecastInfo.list[5].main.temp_max;
+        
+    document.getElementById("tempLo5").innerHTML = forecastInfo.list[5].main.temp_min;
+        
+    document.getElementById("description5").innerHTML = forecastInfo.list[5].weather["0"].description;
+
+    document.getElementById("speed5").innerHTML = forecastInfo.list[5].wind.speed;
+}
+//end of the forecast function
